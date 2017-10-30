c279abf477a771f3f74eda08595f41cc795600e8
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-10-30 22:21:40 +0900

    working through chapter 11 on web scraping

---
+++ mapit.py
@@ -0,0 +1,10 @@
+import webbrowser, sys, pyperclip
+
+# Check if command line arguments were passed
+if len(sys.argv) > 1:
+    #['mapit.py', '870', 'Valencia', 'St.'] --> '870 Valencia St'
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
